c7967cab31ce38e5b6aa4e1e53b2d22c7a9086c1
Luka Rizzi <lukafranco2000@hotmail.com>
2025-04-12 04:36:56 -0300

    ending

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -24,8 +24,11 @@ func _play_current_segment():
 			fishingManager.current_fish = 0
 		2:
 			dialogue._start_next_dialogue()
-		3:
-			ending_sequence._play()
+		3: #sequencia final
+			fishingManager.can_fish = true
+			fishingManager.current_fish = 1
+		4:
+			ending_sequence._play(0)
 
 func _lost_fishing_minigame():
 	dialogue.say_custom("ufa amigo no eras un pescador re capo vos? probá de nuevo, dale. asegurate que no se aleje lo suficiente tirando de la caña con el mouse.")
--- ending_sequence.gd
+++ ending_sequence.gd
@@ -1,4 +1,8 @@
 class_name EndingSequence extends Node
 
-func _play():
-	print("hola")
+func _play(value : int):
+	match value:
+		0:
+			pass
+		1:
+			pass
